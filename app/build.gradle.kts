de5c78a1957e8b4760c330cfe0079651d997c1d5
violetsproject1-pixel <violetsproject1@gmail.com>
2026-08-29 22:21:05 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
 }
