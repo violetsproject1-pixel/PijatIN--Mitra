6da1b575def941ce69c927269d85d113f959cf3c
violetsproject1-pixel <violetsproject1@gmail.com>
2026-09-02 20:31:33 +0700

    Update mainActivity.java

--- app/src/main/java/com/pijatin/rumahaja/mitra/mainActivity.java
+++ app/src/main/java/com/pijatin/rumahaja/mitra/mainActivity.java
@@ -3,13 +3,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-public class MainActivity extends Activity {
-    @Override protected void onCreate(Bundle b){
-        super.onCreate(b);
-        WebView w = new WebView(this);
-        w.getSettings().setJavaScriptEnabled(true);
-        w.setWebViewClient(new WebViewClient());
-        w.loadUrl("file:///android_asset/index.html");
-        setContentView(w);
-    }
+public class MainActivity extends Activity{
+@Override protected void onCreate(Bundle s){
+super.onCreate(s);
+WebView w=new WebView(this);
+w.getSettings().setJavaScriptEnabled(true);
+w.setWebViewClient(new WebViewClient());
+w.loadUrl("file:///android_asset/index.html");
+setContentView(w);
+}
 }
